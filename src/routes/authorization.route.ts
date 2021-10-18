e46de1a43341fd52e79f2d74b1fe28ca00ac7d48
Gilson Alves <gilsonalvesdesouza@hotmail.com>
2021-10-18 19:31:01 -0300

    decodificando o token recebido e validando credenciais

--- src/routes/authorization.route.ts
+++ src/routes/authorization.route.ts
@@ -3,22 +3,31 @@ import ForbiddenError from "../models/errors/forbidden.error.model";
 
 const authorizationRoute = Router();
 
-authorizationRoute.post("/token", (req: Request, res: Response, next: NextFunction)=>{
+authorizationRoute.post("/token", (req: Request, res: Response, next: NextFunction) => {
 
     try {
         const autorizationHeader = req.headers['authorization'];
-    
-        if(!autorizationHeader){
+
+        if (!autorizationHeader) {
             throw new ForbiddenError("Credenciais não informadas");
         }
 
         const [authenticationType, token] = autorizationHeader.split(' ');
 
-        if (authenticationType !== 'Basic' || !token){
+        if (authenticationType !== 'Basic' || !token) {
             throw new ForbiddenError("Tipo de authenticação inválido");
         }
 
-        
+        const tokenContent = Buffer.from(token, 'base64').toString('utf-8');
+
+        const [username, password] = tokenContent.split(":");
+
+        if(!username || !password){
+            throw new ForbiddenError("Credenciais não preenchidas");
+        }
+
+        console.log();
+
     } catch (error) {
         next(error);
     }
